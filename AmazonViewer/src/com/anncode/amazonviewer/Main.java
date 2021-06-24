40b16f18e823383d69cccfc34e410d37f4b65b69
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-24 05:26:16 -0500

    Stream and Filter

--- AmazonViewer/src/com/anncode/amazonviewer/Main.java
+++ AmazonViewer/src/com/anncode/amazonviewer/Main.java
@@ -291,8 +291,13 @@ public class Main {
 		report.setNameFile("reporte");
 		report.setExtension("txt");
 		report.setTitle(":: VISTOS ::");
-		String contentReport = "";
+		StringBuilder contentReport = new StringBuilder();
 		
+		movies.stream()
+		.filter(m -> m.getIsViewed())
+		.forEach(m -> contentReport.append(m.toString() + "\n"));
+		
+/*
 		for (Movie movie : movies) {
 			if (movie.getIsViewed()) {
 				contentReport += movie.toString() + "\n";
@@ -317,8 +322,9 @@ public class Main {
 				
 			}
 		}
+*/
 
-		report.setContent(contentReport);
+		report.setContent(contentReport.toString());
 		report.makeReport();
 		System.out.println("Reporte Generado");
 		System.out.println();
