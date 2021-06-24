1cc0f1b8255f9a4f1e15ff72e913246cad6b69c3
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-23 22:30:15 -0500

    Insert in Java

--- AmazonViewer/src/com/anncode/amazonviewer/dao/MovieDAO.java
+++ AmazonViewer/src/com/anncode/amazonviewer/dao/MovieDAO.java
@@ -14,13 +14,26 @@ import com.anncode.amazonviewer.model.Movie;
 public interface MovieDAO extends IDBConnection {
 	
 	default Movie setMovieViewed(Movie movie) {
+		try (Connection connection = connectToDB()) {
+			Statement statement = connection.createStatement();
+			String query = "INSERT INTO " + TVIEWED + 
+					" ("+TVIEWED_IDMATERIAL+", "+TVIEWED_IDELEMENT+", "+TVIEWED_IDUSUARIO+")" +
+					" VALUES("+ID_TMATERIALS [0]+", "+movie.getId()+", "+TUSER_IDUSUARIO+")";
+			if (statement.executeUpdate(query) > 0) {
+				System.out.println("Se marcó en Visto");
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return movie;
 	}
 	
 	default ArrayList<Movie> read() {
 		ArrayList<Movie> movies = new ArrayList();
 		try (Connection connection = connectToDB()) {
-			String query = "SELECT*FROM" + TMOVIE;
+			String query = "SELECT * FROM " + TMOVIE;
 			PreparedStatement preparedStatement = connection.prepareStatement(query);
 			ResultSet rs = preparedStatement.executeQuery();
 			while (rs.next()) {
@@ -33,6 +46,11 @@ public interface MovieDAO extends IDBConnection {
 						);
 				
 				movie.setId(Integer.valueOf(rs.getString(TMOVIE_ID)));
+				movie.setViewed(getMovieViewed(
+						preparedStatement, 
+						connection, 
+						Integer.valueOf(rs.getString(TMOVIE_ID)))
+					);
 				movies.add(movie);
 			}
 			
@@ -42,7 +60,28 @@ public interface MovieDAO extends IDBConnection {
 		return movies;
 	}
 	
-	private boolean getMovieViewed() {
-		return false;
+	private boolean getMovieViewed(PreparedStatement preparedStatement, Connection connection, int id_movie) {
+		boolean viewed = false;
+		String query = "SELECT * FROM " + TVIEWED + 
+				" WHERE " + TVIEWED_IDMATERIAL + "= ?" +
+				" AND " + TVIEWED_IDELEMENT + "= ?" +
+				" AND " + TVIEWED_IDUSUARIO + "= ?";
+		ResultSet rs = null;
+		
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, ID_TMATERIALS[0]);
+			preparedStatement.setInt(2, id_movie);
+			preparedStatement.setInt(3, TUSER_IDUSUARIO);
+			
+			rs = preparedStatement.executeQuery();
+			viewed = rs.next();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return viewed;
 	}
 }
